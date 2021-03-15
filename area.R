0d78e36a59e1bb8ab5a07da43c5729de8a1fce77
Bhargav2310 <bhargavandhe2310@gmail.com>
2021-03-15 22:08:21 +0530

    bhargav

---
+++ area.R
@@ -0,0 +1,23 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: bhargav
+# Created on: 08/03/21
+
+# Areaofcirlce, triangle, square
+
+print('Area of circle: ')
+radius <- as.integer(readline('Enter radius: '))
+area <- pi * radius * radius
+print(area)
+
+print('Area of triangle: ')
+base <- as.integer(readline('Enter base length: '))
+height <- as.integer(readline('Enter base length: '))
+area <- 0.5 * base * height
+print(area)
+
+
+print('Area of square: ')
+side <- as.integer(readline('Enter side length: '))
+area <- side * side
+print(area)
---
+++ prime.R
@@ -0,0 +1,20 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: bhargav
+# Created on: 15/03/21
+
+start <- as.integer(readline('Enter start: '))
+end <- as.integer(readline('Enter end: '))
+
+isPrime <- function(num) {
+  for (i in 2:num) {
+      if (num %% i == 0) {
+        return
+      }
+  }
+  return true;
+}
+
+for (i in start:end) {
+  if (is)
+}
